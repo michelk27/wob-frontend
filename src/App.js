91365ef15ea72c4711247ceb3e442078cbb9ca69
MKcode27 <kanga.michel89@gmail.com>
2023-07-31 12:48:34 -0400

    Sign in and Sign up Routes/ model

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import React from 'react';
 import Frame8 from "./components/Frame8.jsx";
-import {BrowserRouter as Router, Route, Link, BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import SignUp from '../src/components/Access/SignUp.jsx';
 import Authentication5 from "./components/Authentication5/Authentication5.jsx";
 
-const App = () => {
+function App() {
     return (
         <>
-        <BrowserRouter>
-            {/*<div>*/}
-            {/*    <nav>*/}
-            {/*        <ul>*/}
-            {/*            /!*<li>*!/*/}
-            {/*            /!*/!*    <Link to="/signUp">Sign Up</Link>*!/*!/*/}
-            {/*            /!*/!*</li>*!/*!/*/}
-            {/*            /!*/!*<li>*!/*!/*/}
-            {/*            /!*/!*    <Link to="/Authentication5">Sign In</Link>*!/*!/*/}
-            {/*            /!*/!*</li>*!/*!/*/}
-            {/*        </ul>*/}
-            {/*    </nav>*/}
+            <BrowserRouter>
+                <Routes>
+                        <Route path="/signUp" component={SignUp} />
+                        <Route path="/Authentication5" component={Authentication5} />
+                        <Route path ="/" element={<Frame8 />} />
+                        <Route path="/frame8" element={<Frame8 />} />
+                    </Routes>
+            </BrowserRouter>
+        </>
+    )
 
-                {/*<Route path="/signUp" component={SignUp} />*/}
-                {/*<Route path="/Authentication5" component={Authentication5} />*/}
-                <Route path ="/" element={<Frame8 />} />
-                <Route path="/frame8" element={<Frame8 />} />
-    //     //     </div>
-    //     // </Router>
-    // );
-};
+}
 
 export default App;
