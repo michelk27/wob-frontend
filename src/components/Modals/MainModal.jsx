8fe7c39b50cc7a541d50be07a8409d4566581e69
MKcode27 <kanga.michel89@gmail.com>
2023-08-07 03:56:42 -0400

    X/sign in button position

--- src/components/Modals/MainModal.jsx
+++ src/components/Modals/MainModal.jsx
@@ -4,8 +4,16 @@ function MainModal(props) {
 
     return (
         <div className="fixed top-0 left-0 w-screen min-h-screen bg-black-opacity-60 z-10 flex justify-center items-center">
-            <div className="w-[998px] rounded-lg bg-white p-4 z-20 ">
-                <div className="w-full flex justify-end items-center ">
+            <div className="w-[998px] rounded-[12px] bg-white p-4 z-20 relative ">
+                <div className="flex flex-col py-2">
+                    <label>Username</label>
+                    <input className="rounded-lg border p-2" type="text"/>
+                </div>
+                <div className="flex flex-col py-2">
+                    <label>Password</label>
+                    <input className="rounded-lg border p-2" type="text"/>
+                </div>
+                <div className="absolute top-2 right-2 font-bold">
                     <button className="text-right font-bold " onClick={props.onClose}>X</button>
                 </div>
                 <div className="w-full">
--- src/components/Template/LoginTemplate.jsx
+++ src/components/Template/LoginTemplate.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import MainModal from "../Modals/MainModal.jsx";
+import PrimaryButton from "../Buttons/PrimaryButton.jsx";
 
 function LoginTemplate(props) {
 
     return (
         <MainModal onClose={props.onClose}>
-            login form
+            <div className="w-[416px] h-[74px] flex justify-center items-center mx-auto">
+                <PrimaryButton label="Sign in" onClick={props.open}  />
+            </div>
         </MainModal>
 
     );
