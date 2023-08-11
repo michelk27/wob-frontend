c086787b2fe75a32e8e71a73adfe2a2cd80cb8c9
MKcode27 <kanga.michel89@gmail.com>
2023-08-11 09:30:25 -0400

    newvisit page text fields

--- src/components/NewVisitPage.jsx
+++ src/components/NewVisitPage.jsx
@@ -1,29 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LogoImage from "../assets/logo.svg";
 
 const NewVisitPage = () => {
-    return(
-        <div className="p-8">
-            <div className="w-[1920px] h-[91px] shadow-md bg-white flex justify-center items-center ">
-            <img src={LogoImage} alt="logo" className="h-[74px] w-[250px] my-0" />
-            </div>
-            <div className="flex flex-col py-2">
-                <label>Company name:</label>
-                <input className="rounded-lg border p-2" type="text"/>
-            </div>
-            <div className="flex flex-col py-2">
-            <label>Type the company address:</label>
-            <input className="rounded-lg border p-2" type="text"/>
-            </div>
-            <div className="flex flex-col py-2">
-            <label>Choose a date:</label>
-            <input className="rounded-lg border p-2" type="text"/>
-            </div>
-            <div>
-                <button className="bg-primary rounded-md px-6 py-2 text-white mt-4" >Request a Service</button>
-            </div>
-      </div>
-    )
-}
-export default NewVisitPage;
+    const [companyName, setCompanyName] = useState("");
+    const [addresses, setAddresses] = useState("");
+    const [date, setDate] = useState("");
+    const [type, setType] = useState("");
+    const API_URL_REQUEST =  "http://localhost:8080/api/visit-requests"
 
+
+    const handleRequestVisit = async () => {
+        const initialStatus = "Pending"; //  initial status
+        try {
+            const response = await fetch(API_URL_REQUEST, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    companyName,
+                    type,
+                    addresses,
+                    date,
+                    status: initialStatus,
+                }),
+            });
+
+            if (response.ok) {
+                // Request successful, you can handle it here
+                console.log('Visit requested successfully');
+            } else {
+                // Request failed, you can handle the error here
+                console.error('Failed to request visit');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        <div>
+            <div className="w-[1920px] h-[91px] shadow-md bg-white flex justify-center items-center ">
+                <img src={LogoImage} alt="logo" className="h-[74px] w-[250px] my-0" />
+            </div>
+            <h1 className="text-[#0F666D] font-bold px-8">Request a Service</h1>
+            <div className="p-8">
+                <div className="flex flex-col py-2">
+                    <label>Company name:</label>
+                    <input
+                        className="rounded-lg border p-2"
+                        type="text"
+                        value={companyName}
+                        onChange={(e) => setCompanyName(e.target.value)}
+                    />
+                </div>
+                <div className="flex flex-col py-2">
+                    <label>Type the company address:</label>
+                    <input
+                        className="rounded-lg border p-2"
+                        type="text"
+                        value={addresses}
+                        onChange={(e) => setAddresses(e.target.value)}
+                    />
+                </div>
+                <div className="flex flex-col py-2">
+                    <label>Choose a date:</label>
+                    <input
+                        className="rounded-lg border p-2"
+                        type="date"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                    />
+                </div>
+                <div className="flex flex-col py-2">
+                    <label>Visit Type:</label>
+                    <input
+                        className="rounded-lg border p-2"
+                        type="text"
+                        value={type}
+                        onChange={(event) => {
+                            setType(event.target.value);
+                        }}
+                    />
+                </div>
+                <div className="flex justify-center mt-4">
+                    <button
+                        className="bg-primary rounded-md px-6 py-2 text-white mt-4"
+                        onClick={handleRequestVisit}
+                    >
+                        Request a Visit
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NewVisitPage;
--- src/components/Template/VisitRequestsList.jsx
+++ src/components/Template/VisitRequestsList.jsx
@@ -22,7 +22,7 @@ function VisitRequestsList() {
 
     return (
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg px-6">
-            <h1>My Service Requests</h1>
+            <h1 className=" text-[#0F666D] font-bold ">My Service Requests</h1>
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 border p-8 ">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
